35fe78de7138c64c0aad673b12fca2d78602ca04
Henadzi Borodavitsyn (GeDaY) <gedaskyev@gmail.com>
2021-12-05 17:05:52 +0300

    Edit .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,13 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'standard',
-        'plugin:import/recommended',
-        'airbnb',
-        'parser',
-        'babel-eslint',
-        'prettier',
-    ],
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    rules: {},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard', 'plugin:import/recommended', 'airbnb', 'prettier'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {},
 }
